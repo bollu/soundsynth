9f91a475a203195aa480393d2ca9635e51063e27
Siddharth Bhat <siddu.druid@gmail.com>
2021-04-17 23:48:56 +0530

    get cmake to compile ch3. Unfortunately, I don't know how to get it to copy the wav files I need

--- ch3/AudioEventMgr.cpp
+++ ch3/AudioEventMgr.cpp
@@ -43,23 +43,11 @@ int playBufferInt16(float *audio, int numSamples, int rate, int channels, int se
 	snd_pcm_t *pcm_handle;
 	snd_pcm_hw_params_t *params;
 	snd_pcm_uframes_t frames;
-	char *buff;
-	int buff_size, loops;
 
 	int16 *pData = new int16[numSamples];
 	for(int nIndex = 0; nIndex < numSamples; ++nIndex) {
 		ConvertFloatToAudioSample16(pRawData[nIndex],pData[nIndex]);
 	}
-
-	// if (argc < 4) {
-	// 	printf("Usage: %s <sample_rate> <channels> <seconds>\n",
-	// 							argv[0]);
-	// 	return -1;
-	// }
-
-	// rate 	 = atoi(argv[1]);
-	// channels = atoi(argv[2]);
-	// seconds  = atoi(argv[3]);
 
 	printf("rate: %d | channels: %d | seconds: %d\n", rate, channels, seconds);
 
@@ -71,33 +59,36 @@ int playBufferInt16(float *audio, int numSamples, int rate, int channels, int se
 
 	/* Allocate parameters object and fill it with default values*/
 	snd_pcm_hw_params_alloca(&params);
-
 	snd_pcm_hw_params_any(pcm_handle, params);
 
 	/* Set parameters */
+	// why do I need read? Oh I see, because the other way to access it is by `mmap()`.
 	if ((pcm = snd_pcm_hw_params_set_access(pcm_handle, params,
 					SND_PCM_ACCESS_RW_INTERLEAVED)) < 0) 
 		printf("ERROR: Can't set interleaved mode. %s\n", snd_strerror(pcm));
-
+	
+	// 16 bit
 	if ((pcm = snd_pcm_hw_params_set_format(pcm_handle, params,
 						SND_PCM_FORMAT_S16_LE) < 0))
 		printf("ERROR: Can't set format. %s\n", snd_strerror(pcm));
 
+	// two / one channel
 	if ((pcm = snd_pcm_hw_params_set_channels(pcm_handle, params, channels) < 0)) 
 		printf("ERROR: Can't set channels number. %s\n", snd_strerror(pcm));
 
+	// ask for rate to be close to `rate`.
 	if ((pcm = snd_pcm_hw_params_set_rate_near(pcm_handle, params, (unsigned int *)&rate, 0) < 0))
 		printf("ERROR: Can't set rate. %s\n", snd_strerror(pcm));
 
-	/* Write parameters */
+	// set HW params.
 	if ((pcm = snd_pcm_hw_params(pcm_handle, params) < 0))
 		printf("ERROR: Can't set harware parameters. %s\n", snd_strerror(pcm));
 
-	/* Resume information */
+	// print information, check that it's assigned correctly.
 	printf("PCM name: '%s'\n", snd_pcm_name(pcm_handle));
 	printf("PCM state: %s\n", snd_pcm_state_name(snd_pcm_state(pcm_handle)));
 	snd_pcm_hw_params_get_channels(params, &tmp);
-	printf("channels: %i ", tmp);
+	printf("channels: %d ", tmp);
 
 	if (tmp == 1)
 		printf("(mono)\n");
@@ -109,25 +100,32 @@ int playBufferInt16(float *audio, int numSamples, int rate, int channels, int se
 
 
 	/* Allocate buffer to hold single period */
+	// Extract period size from a configuration space.
+	// params	Configuration space
+	// val	Returned approximate period size in frames
+	// dir	Sub unit direction
+	// Returns
+	// 0 otherwise a negative error code if the configuration space does not contain a single value
+	// Actual exact value is <,=,> the approximate one following dir (-1, 0, 1) 
 	snd_pcm_hw_params_get_period_size(params, &frames, 0);
 
-	buff_size = frames * channels /* 2 -> sample size */;
-	buff = (char *) malloc(buff_size * sizeof(int16) );
+	int nelem_framebuf = frames * channels /* 2 -> sample size */;
+	char *buff = (char *) malloc(nelem_framebuf * sizeof(int16) );
 
 	snd_pcm_hw_params_get_period_time(params, &tmp, NULL);
     
     int ix = 0;
 	while (ix < numSamples) {
 	// for (loops = (seconds * 1000000) / tmp; loops > 0; loops--) {
-		// for (int i = 0; i < buff_size/sizeof(int); i++) {
+		// for (int i = 0; i < nelem_framebuf/sizeof(int); i++) {
 		// 	memcpy(buff + i *sizeof(int), pData + ix + i, sizeof(int)); 
 		// 	ix++;
 		// }
 		
-        for(int i = 0; i < buff_size; ++i) {
+        for(int i = 0; i < nelem_framebuf; ++i) {
             memcpy(buff + i * sizeof(int16), & pData[ix+i], sizeof(int16));
         }
-		ix += buff_size; 
+		ix += nelem_framebuf; 
 		
         /*if ((pcm = fread(buff, 1, buff_size, f) == 0)) {
 			printf("Early end of file.\n");
